254fbd40d8c1545989d1387bed8267395a796fc3
Luca Zanetti <78453748+elzanetoide32@users.noreply.github.com>
2024-04-09 17:00:38 -0300

    Add files via upload

---
+++ html/animacion.js
@@ -0,0 +1,28 @@
+function apretar(){
+	alert ("me estas apretando");
+};
+
+
+function encima(){
+	alert("estas encima mioooo");
+};
+
+
+function afuera(){
+	alert("otra pagina");
+};
+
+
+
+/*var tiempo =setInterval(
+	function()
+	{document.write("TExto de ejemplo <br>")},
+	100
+	);*/
+
+
+
+
+
+
+
